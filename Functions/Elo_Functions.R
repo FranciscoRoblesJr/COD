da678119914ba56710a11915f1080a0fe2720268
FranciscoRoblesJr <franciscorobles35@gmail.com>
2019-06-03 00:41:23 -0600

    functions done

--- Functions/Elo_Functions.R
+++ Functions/Elo_Functions.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(magrittr)
 
 #### Functions ####
+### Create new Player ###
 Create_Player = function(Player){
   # Set initial values
   Name = deparse(substitute(Player))
@@ -18,10 +19,9 @@ Create_Player = function(Player){
   
   
   # Compile into lists
-  hp.df = data.frame(Date = d, Elo = HP_Elo)
-  snd.df = data.frame(Date = d, Elo = SnD_Elo)
-  control.df = data.frame(Date = d, Elo = Control_Elo)
-  rownames(hp.df)[1] = 'Initial' ; rownames(snd.df)[1] = 'Initial' ; rownames(control.df)[1] = 'Initial'
+  hp.df = data.frame(Date = d, Place = '', Opponent = '', Map = '', Elo = HP_Elo, stringsAsFactors = F)
+  snd.df = data.frame(Date = d, Place = '', Opponent = '', Map = '', Elo = SnD_Elo,  stringsAsFactors = F)
+  control.df = data.frame(Date = d, Place = '', Opponent = '', Map = '', Elo = Control_Elo,  stringsAsFactors = F)
   elo.list = list(Hardpoint = hp.df, 'Search and Destroy' = snd.df, Control = control.df)
   
   
@@ -30,7 +30,7 @@ Create_Player = function(Player){
   return(return.list)
 }
 
-
+### Elo Probability ###
 Elo_Probability = function(score1, score2){
   # This function returns the probability a team will win based on both team's scores
   # It takes two teams scores and returns list of teams scores as well as the Probability Team1 wins
@@ -40,7 +40,7 @@ Elo_Probability = function(score1, score2){
   return(return.list)
 }
 
-
+### Get Player Elo ###
 get_Elo = function(Player){
   # Gets Elo Rating for individual player and returns vector of the 3 game modes
   Name = deparse(substitute(Player))
@@ -50,14 +50,104 @@ get_Elo = function(Player){
     Name = Player
   }
   
-  Elo.df = Player.List %>% extract2(Name) %>% extract2('Elo') %>% map(2)
-  HP.Elo = Elo.df %>% extract2(1)
-  SND.Elo = Elo.df %>% extract2(2)
-  Control.Elo = Elo.df %>% extract2(3)
+  Elo.list = Player.List %>% extract2(Name) %>% extract2('Elo') %>% map('Elo')
+  HP.Elo = Elo.list %>% extract2(1)
+  SND.Elo = Elo.list %>% extract2(2)
+  Control.Elo = Elo.list %>% extract2(3)
   
   Elo.Vec = c(last(HP.Elo), last(SND.Elo), last(Control.Elo))
   
   return(Elo.Vec)
+}
+
+### Get Team Elo ###
+get_TeamElo = function(players_names){
+  elo.list = list()
+  for(i in players_names){
+    elo.list[[i]] = get_Elo(i)
+  }
+  hp.elo = map(elo.list, 1) %>% unlist()
+  snd.elo = map(elo.list, 2)  %>% unlist()
+  control.elo = map(elo.list, 3)  %>% unlist()
+  all.elo = c(mean(hp.elo), mean(snd.elo), mean(control.elo))
+  
+  return(all.elo)
+}
+
+### Update Player Elo for Series ###
+update_PlayerElo = function(teams, all_players, results, place){
+  # set up data.frames
+  num.games = length(results) / 2
+  elo.index = c(1, 2, 3, 1, 2)[1:num.games]
+  d = Sys.Date()
+  t1.players = all_players[1:5]
+  t2.players = all_players[6:10]
+  
+  k = 32
+  elo.list = list()
+  for(i in 1:num.games){
+    # Calculate Elo change for that game
+    team1.elo = get_TeamElo(t1.players) %>% extract2(elo.index[i])
+    team2.elo = get_TeamElo(t2.players) %>% extract2(elo.index[i])
+    elo.list[[i]] = Elo_Probability(team1.elo, team2.elo)
+    prob = c(elo.list[[i]]$Probability, 1 - elo.list[[i]]$Probability)
+    winner = results[c((2*i)-1, 2*i)]
+    change = k*(winner - prob)
+    
+    # Change the player elo's
+    # Add in COD Rating to new.elo line for both for loops
+    for(j in 1:length(t1.players)){
+      df = Player.List %>% extract2(t1.players[j]) %>% extract2('Elo') %>% extract2(elo.index[i])
+      prev.elo = get_Elo(t1.players[j]) %>% extract2(elo.index[i])
+      new.elo = prev.elo + change[1]
+      
+      new.row = list(Date = d, Place = place, Opponent = teams[2], Map = paste0('Map', i), Elo = new.elo)
+      df = rbind.data.frame(df, new.row)
+      Player.List[[t1.players[j]]][['Elo']][[elo.index[i]]] <<- df
+    }
+    for(l in 1:length(t2.players)){
+      df = Player.List %>% extract2(t2.players[l]) %>% extract2('Elo') %>% extract2(elo.index[i])
+      prev.elo = get_Elo(t2.players[l]) %>% extract2(elo.index[i])
+      new.elo = prev.elo + change[2]
+      
+      new.row = list(Date = d, Place = place, Opponent = teams[1], Map = paste0('Map', i), Elo = new.elo)
+      df = rbind.data.frame(df, new.row, stringsAsFactors = F)
+      Player.List[[t2.players[l]]][['Elo']][[elo.index[i]]] <<- df
+    }
+  }
+  return(elo.list)
+}
+
+Series = function(file_path, place){
+  # only put something by wins
+  series = read.csv(file_path, header = F)
+  place = deparse(substitute(place))
+  
+  
+  # get series data
+  teams = series[c(1,9), 1] %>% as.character()
+  players = series[c(3:7, 11:15), 1] %>% as.character()
+  t1.players = players[1:5]
+  t2.players = players[6:10]
+  num.games = nrow(series) / 16
+  results = (!(series[seq(1, nrow(series), 8), 2] == '')) %>% as.numeric()
+  elo.index = c(1, 2, 3, 1, 2)[1:num.games]
+  
+  
+  # Create new players that aren't in Player.List
+  all.players = names(Player.List)
+  '%nin%' = Negate('%in%') # not in
+  for(i in 1:length(players)){
+    if(players[i] %nin% all.players){
+      Player.List[[players[i]]] <<- Create_Player(players[i])
+    }
+  }
+  
+  Match.Elo.Results = update_PlayerElo(teams = teams, all_players = players, results = results, place = place)
+  
+  # Create object that saves Match Elo Results and have it save directly in funciton
+  # Wont need to return anything at that point
+  return(Match.Elo.Results)
 }
 
 
@@ -95,108 +185,6 @@ wElo_Standings = function(){
   return(Elo.df)
 }
 
-
-get_TeamElo = function(players_names){
-  elo.list = list()
-  for(i in players_names){
-    elo.list[[i]] = get_Elo(i)
-  }
-  hp.elo = map(elo.list, 1) %>% unlist()
-  snd.elo = map(elo.list, 2)  %>% unlist()
-  control.elo = map(elo.list, 3)  %>% unlist()
-  all.elo = c(mean(hp.elo), mean(snd.elo), mean(control.elo))
-  
-  return(all.elo)
-}
-
-update_PlayerElo = function(Player, elo.change, opponent){
-  Name = deparse(substitute(Player))
-  if(Name %in% names(Player.List)){
-    Name = Name
-  } else {
-    Name = Player
-  }
-  
-  # set up data.frames
-  num.games = length(elo.change)
-  elo.index = c(1, 2, 3, 1, 2)[1:num.games]
-  d = Sys.Date()
-  
-  
-  for(i in 1:num.games){
-    df = Player.List %>% extract2(Name) %>% extract2('Elo') %>% extract2(elo.index[i])
-    prev.elo = get_Elo(Name)[elo.index[i]]
-    new.elo = prev.elo + elo.change[i]
-    
-    date.col = c(df[, 'Date'], d)
-    elo.col = c(df[,'Elo'], new.elo)
-    df.rownames = c(rownames(df), paste(d, opponent, paste0('Map', i)))
-    
-    new.df = cbind.data.frame(Date = date.col, Elo = elo.col)
-    rownames(new.df) = df.rownames
-    
-    Player.List[[Name]][['Elo']][[elo.index[i]]] <<- new.df
-  }
-}
-
-Series = function(file_path){
-  # only put something by wins
-  series = read.csv(file_path, header = F)
-  
-  
-  # get series data
-  teams = series[c(1,9), 1] %>% as.character()
-  players = series[c(3:7, 11:15), 1] %>% as.character()
-  t1.players = players[1:5]
-  t2.players = players[6:10]
-  num.games = nrow(series) / 16
-  results = (!(series[seq(1, nrow(series), 8), 2] == '')) %>% as.numeric()
-  elo.index = c(1, 2, 3, 1, 2)[1:num.games]
-  
-  
-  # Create new players that aren't in Player.List
-  all.players = names(Player.List)
-  '%nin%' = Negate('%in%') # not in
-  for(i in 1:length(players)){
-    if(players[i] %nin% all.players){
-      Player.List[[players[i]]] <<- Create_Player(players[i])
-    }
-  }
-  
-  
-  # Get team elo's
-  team1.elo = get_TeamElo(t1.players)
-  team2.elo = get_TeamElo(t2.players)
-  
-  
-  # Elo simulation w/ k = 32
-  # Make more flexible to be able to save results from match w/ given team elo's
-  k = 32
-  elo.change = list()
-  for(i in 1:num.games){
-    t1.prob = Elo_Probability(team1.elo[(elo.index)[i]], team2.elo[(elo.index)[i]])
-    t2.prob = Elo_Probability(team2.elo[(elo.index)[i]], team1.elo[(elo.index)[i]])
-    prob = c(t1.prob$Probability, t2.prob$Probability)
-    winner = results[c((2*i)-1, 2*i)]
-    elo.change[[i]] = k*(winner - prob)
-  }
-  
-  
-  # Change Player Elo (ADD IN COD RATING FUNCTIONALITY HERE)
-  t1.change = map(elo.change, 1) %>% unlist()
-  t2.change = map(elo.change, 2) %>% unlist()
-  
-  for(i in t1.players){
-    update_PlayerElo(i, t1.change, teams[2])
-  }
-  for(j in t2.players){
-    update_PlayerElo(j, t2.change, teams[1])
-  }
-  return(team1.elo)
-}
-
-Elo_Probability(c(10, 20, 30), c(30, 20, 10))
-test = Series('C:/Users/franc/Downloads/test geng uyu 2-3  - Sheet1.csv')
 
 
 #### Ideas
---
+++ Series_Scores.R
@@ -0,0 +1,17 @@
+source('Functions/Elo_Functions.R')
+Player.List = readRDS('Objects/Player.List')
+
+
+#### PL Week 1 - Week 4
+
+
+#### Fort Worth
+Series('Data/Fort Worth/FW - 1_e6_rr.csv', Fort_Worth)
+Series('Data/Fort Worth/FW - 2_midnight_splyce.csv', Fort_Worth)
+Series('Data/Fort Worth/FW - 3_excel_lg.csv', Fort_Worth)
+#Series('Data/Fort Worth/FW - 4_100t_eg.csv', Fort_Worth)
+Series('Data/Fort Worth/FW - 5_geng_heretics.csv', Fort_Worth)
+Series('Data/Fort Worth/FW - 6_eu_nv.csv', Fort_Worth)
+Series('Data/Fort Worth/FW - 7_og_rec.csv', Fort_Worth)
+Series('Data/Fort Worth/FW - 8_denial_uyu.csv', Fort_Worth)
+
